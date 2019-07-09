f96e9483788610b7f453c2891e8959f6304cc0d1
Aneesh Nelavelly <35740650+Aneeshn25@users.noreply.github.com>
2019-07-08 20:00:22 -0400

    new  vpc

---
+++ high_availability/dev.tfvars
@@ -0,0 +1,2 @@
+#private_subnet = ["10.0.0.0/26", "10.0.0.64/26"]
+#private_name = []
---
+++ high_availability/variable.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+	default = "10.0.0.0/24"
+}
+
+variable "subnet_cidrs_public" {
+  type    = "list"
+  default = ["10.0.0.0/26", "10.0.0.64/26"]
+}
+
+variable "subnet_cidrs_private" {
+  type    = "list"
+  default = ["10.0.0.128/26", "10.0.0.192/26"]
+}
+
+variable "public_name" {
+	type    = "list"
+	default = ["public-1", "public-2"]
+}
+
+variable "private_name" {
+	type    = "list"
+	default = ["private-1", "private-2"]
+}
+
+variable "availability_zones" {
+  description = "AZs in this region to use"
+  default = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  type = "list"
+}
