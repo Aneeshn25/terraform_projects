690552e1dcfa4a24929ae0590750328a3d24cdd1
Aneesh Nelavelly <35740650+Aneeshn25@users.noreply.github.com>
2019-07-10 00:29:33 -0400

    updated webpage

--- high_availability/app.tpl.sh
+++ high_availability/app.tpl.sh
@@ -4,4 +4,17 @@ sudo apt update
 sudo apt install --yes nginx
 cd /var/www/html
 touch index.html
-sudo echo "Hello! if you are seeing this then this is working." > index.html
+
+echo "<!DOCTYPE html>" >index.html
+echo "<html>" >> index.html
+echo "<head>" >> index.html
+echo "<meta charset="UTF-8">" >> index.html
+echo "<title>Welcome</title>" >> index.html
+echo "</head>" >> index.html
+echo "<body>" >> index.html
+echo "<center><p>-------------------------------------</p></center>" >> index.html
+echo "<center><h1>Hello world</h1></center>" >> index.html
+echo "<center><p>-------------------------------------</p></center>" >> index.html
+echo "<center><p>Hostname--"$HOSTNAME"</p></center>" >> index.html
+echo "</body>" >> index.html
+echo "</html>" >> index.html
