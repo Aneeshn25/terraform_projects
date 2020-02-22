b4fbd8d8a514af9b25d5d8c47efb61e70b05a600
Aneesh Nelavelly <anelavelly@onica.com>
2020-02-22 17:53:51 -0500

    allowed execution form api gateway postit resource

--- api-gateway-terraform/main.tf
+++ api-gateway-terraform/main.tf
@@ -105,6 +105,15 @@ resource "aws_lambda_permission" "apigw_lambda_idItems" {
   source_arn = "arn:aws:execute-api:${var.AWS_REGION}:${var.AWS_ACCOUNT}:${aws_api_gateway_rest_api.onicaTestAPI.id}/*/${aws_api_gateway_method.idnoGetMethod.http_method}${aws_api_gateway_resource.idno-api-resource.path}"
 }
 
+resource "aws_lambda_permission" "apigw_lambda_postit" {
+  statement_id  = "AllowExecutionFromAPIGatewayPostit"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.postItemsToOnicatest.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "arn:aws:execute-api:${var.AWS_REGION}:${var.AWS_ACCOUNT}:${aws_api_gateway_rest_api.onicaTestAPI.id}/*/${aws_api_gateway_method.postitPostMethod.http_method}${aws_api_gateway_resource.postit-api-resource.path}"
+}
+
 #creating api gateway integration for resource "id" GET method 
 resource "aws_api_gateway_integration" "id-lambda-api-integration" {
   rest_api_id = "${aws_api_gateway_rest_api.onicaTestAPI.id}"
@@ -160,7 +169,6 @@ resource "aws_api_gateway_integration" "idno-lambda-api-integration" {
 EOF
   }
 }
-
 
 #creating method response for resource "{idno}" GET method
 resource "aws_api_gateway_method_response" "idno-lambda-api-method-response" {
